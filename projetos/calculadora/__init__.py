ca00f9b266c8b64143c4b1e4390b619d3eff19d8
Cristiane Del Barco <cris.barco@hotmail.com>
2023-04-01 14:03:41 -0300

    envio do init

--- projetos/calculadora/__init__.py
+++ projetos/calculadora/__init__.py
@@ -4,14 +4,24 @@ import funcoes as f
 def calcule():
     a = input("Digite o primeiro número: ")
     b = input("Digite o segundo número: ")
+    try:
+      a = eval(a)  
+    except (NameError, SyntaxError):
+      pass
+    try:
+      b = eval(b)  
+    except (NameError, SyntaxError):
+      pass
     operacao = input("Digite qual operação deseja: ")
 
-    match operacao.lower():
-      case ['soma', '+']:
+    match operacao:
+      case 'soma' | '+':
         print(f.soma(a,b))
-      case ['subtração', '-']:
+      case 'subtração' | '-':
         print(f.subtracao(a,b))
-      case ['divisão', '/']:
+      case 'divisão' | '/':
         print(f.divisao(a,b))
-      case ['multiplicação', '*']:
+      case 'multiplicação' | '*':
         print(f.multiplicacao(a,b))
+      case _:
+        print("Operação não reconhecida!")
